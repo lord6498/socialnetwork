f07dbf3a1e2adb563ba9685e3267e4766504c35d
Nick <>
2021-02-12 15:57:46 +0300

    initial project

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+public class Post {
+    public ru.netology.Post[] getPost() {
+        return Post;
+    }
+
+    public void setPost(ru.netology.Post[] post) {
+        Post = post;
+    }
+
+    private Post[] Post;
+
+    public String generateBlock;
+
+
+}
