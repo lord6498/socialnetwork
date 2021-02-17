1a0d30745e76597155eab398d54dc05535d975ad
Nick <>
2021-02-17 12:32:21 +0300

    Post manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.manager;
+
+import ru.netology.Post;
+
+public class PostManager {
+
+    private Post[] postInfo;
+
+    public Post[] search (int ownerId, String domain, String query, int ownersOnly, int Count, int Offset) {
+        return postInfo;
+    }
+
+    public void delete (int ownerId, int postId){
+        return;
+    }
+
+}
